940236c2c9b19e4d098e0671c73c63ed5210bfc7
Giancarllo Rojas <giancarllor@gmail.com>
2019-05-23 00:57:33 -0300

    Basic layout

---
+++ layouts/default.vue
@@ -0,0 +1,219 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="true"
+      fixed
+      app
+      :mobile-break-point="600"
+      class="box-shadow"
+    >
+      <v-list >
+        <v-tooltip :close-delay="0" :open-delay="0" v-for="(item, i) in items" :key="i" right>
+          <template v-slot:activator="{ on }">
+
+            <v-list-tile
+              :class="{'auto-height': item.divider}"
+              :to="item.to"
+              v-on="(miniVariant == false) ? {} : on"
+              router
+              exact
+            >
+              <v-divider class="my-1 pa-0" v-if="item.divider"></v-divider>
+              <v-list-tile-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title v-text="item.title" />
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+          <span>{{item.title}}</span>
+        </v-tooltip>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar
+      :clipped-left="true"
+      fixed
+      app
+      dense
+      
+      :color="color.value"
+      :dark="color.dark"
+      
+    >
+        <v-btn
+          icon
+          @click.stop="changeVariant"
+        >
+          <v-fade-transition leave-absolute>
+            <v-icon v-if="miniVariant == true || drawer == false">menu</v-icon>
+          </v-fade-transition>
+          <v-fade-transition leave-absolute>
+            <v-icon v-if="miniVariant == false && drawer == true">arrow_back</v-icon>
+          </v-fade-transition>
+        </v-btn>
+
+      <nuxt-link to="/">
+        <v-toolbar-title class="white--text" v-text="title" />
+      </nuxt-link>
+      <v-spacer />
+      <v-btn
+        icon
+        v-if="false"
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>menu</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-tile @click.native="right = !right">
+          <v-list-tile-action>
+            <v-icon light>compare_arrows</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [
+        {
+          icon: 'apps',
+          title: 'Início',
+          to: '/'
+        },
+        {divider: true},
+        {
+          icon: 'fa fa-bullhorn',
+          title: 'Campanhas',
+          to: '/campanhas'
+        },
+        {
+          icon: 'fas fa-table',
+          title: 'Tablóides',
+          to: '/tabloides'
+        },
+        {
+          icon: 'fa fa-puzzle-piece',
+          title: 'Produtos',
+          to: '/produtos'
+        },
+        {divider: true},
+        {
+          icon: 'fas fa-users-cog',
+          title: 'Usuários',
+          to: '/usuarios'
+        },
+        {
+          icon: 'fas fa-sign-out-alt',
+          title: 'Sair',
+          to: '/sair'
+        },
+      ],
+      miniVariant: true,
+      right: true,
+      rightDrawer: false,
+      title: 'encarteDO!',
+      color: {
+        value: 'blue darken-2',
+        dark: true
+      }
+    }
+  },
+
+  watch: {
+    $route(to, from){
+      this.miniVariant = true
+
+      this.defineColor(to.path)
+    }
+  },
+
+  created(){
+    if(window.innerWidth < 600){
+      this.drawer = false
+    }else{
+      this.drawer = true
+    }
+
+    this.defineColor(this.$route.path)
+  },
+
+  methods: {
+    changeVariant(){
+      if(window.innerWidth < 600){
+        this.miniVariant = false
+        this.drawer = true
+      }else{
+        this.miniVariant = !this.miniVariant
+      }
+    },
+
+    defineColor(path){
+      console.log(path)
+      if(path.includes('campanhas')){
+        this.color = {
+          value: 'indigo darken-2',
+          dark: true
+        }
+      }
+      else if(path.includes('tabloides')){
+        this.color = {
+          value: 'cyan darken-2',
+          dark: true
+        }
+      }
+      else if(path.includes('produtos')){
+        this.color = {
+          value: 'grey darken-2',
+          dark: true
+        }
+      }
+      else if(path.includes('usuarios')){
+        this.color = {
+          value: 'blue-grey darken-2',
+          dark: true
+        }
+      }
+      else {
+        this.color = {
+          value: 'blue darken-2',
+          dark: true
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.box-shadow
+  box-shadow: 0px 5px 15px rgba(0,36,79,0.1) !important;
+.auto-height
+  div
+    height: auto !important
+</style>
---
+++ pages/campanhas/index.vue
@@ -0,0 +1,216 @@
+<template>
+  <div>
+    <div class="elevation-1">
+      <v-toolbar flat color="white">
+        <v-toolbar-title>
+          <v-layout align-center class="mr-2">
+            <v-img class="mr-2" width="40" src="/icones/campanhas.png"></v-img>
+            Campanhas
+          </v-layout>
+        </v-toolbar-title>
+        <v-divider
+          class="mx-2"
+          inset
+          vertical
+        ></v-divider>
+        <span class="subheading">Listagem</span>
+        <v-spacer></v-spacer>
+        <v-btn color="success">
+          <v-icon class="mr-2">far fa-plus-square</v-icon>
+          Adicionar campanha
+        </v-btn>
+      </v-toolbar>
+      <v-data-table
+        :headers="headers"
+        :items="items"
+      >
+        <template v-slot:items="props">
+          <td>{{ props.item.name }}</td>
+          <td class="text-xs-right">{{ props.item.calories }}</td>
+          <td class="text-xs-right">{{ props.item.fat }}</td>
+          <td class="text-xs-right">{{ props.item.carbs }}</td>
+          <td class="text-xs-right">{{ props.item.protein }}</td>
+          <td class="justify-center layout px-0">
+            <v-icon
+              small
+              class="mr-2"
+              @click="editItem(props.item)"
+            >
+              edit
+            </v-icon>
+            <v-icon
+              small
+              @click="deleteItem(props.item)"
+            >
+              delete
+            </v-icon>
+          </td>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize">Reset</v-btn>
+        </template>
+      </v-data-table>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      dialog: false,
+      headers: [
+        {
+          text: 'Dessert (100g serving)',
+          align: 'left',
+          sortable: false,
+          value: 'name'
+        },
+        { text: 'Calories', value: 'calories' },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Actions', value: 'name', sortable: false }
+      ],
+      items: [],
+      bread: [
+        
+      ],
+      editedIndex: -1,
+      editedItem: {
+        name: '',
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0
+      },
+      defaultItem: {
+        name: '',
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0
+      }
+    }),
+
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+      }
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      }
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      initialize () {
+        this.items = [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7
+          }
+        ]
+      },
+
+      editItem (item) {
+        this.editedIndex = this.items.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        const index = this.items.indexOf(item)
+        confirm('Are you sure you want to delete this item?') && this.items.splice(index, 1)
+      },
+
+      close () {
+        this.dialog = false
+        setTimeout(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        }, 300)
+      },
+
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.items[this.editedIndex], this.editedItem)
+        } else {
+          this.items.push(this.editedItem)
+        }
+        this.close()
+      }
+    }
+  }
+</script>
