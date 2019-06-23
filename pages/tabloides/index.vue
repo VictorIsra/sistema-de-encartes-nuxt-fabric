88073b157137a74f8b7774a7dd6b424034b2fee6
victor israel <victorzen2013@gmail.com>
2019-06-23 00:11:15 -0300

    canvas posicionado na gambiarra...preciso aprender a mexer com css...

--- pages/tabloides/index.vue
+++ pages/tabloides/index.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
         <v-btn color="success" @click="criaRet">testa</v-btn>
-        <canvas id="c"></canvas>
+        <div class="text-xs-center">
+            <canvas id="c" class="canvas" width="700px" height="900px"></canvas>
+        </div>    
+        <img ref="img" src="https://upload.wikimedia.org/wikipedia/pt/thumb/a/ab/Scorpion.png/200px-Scorpion.png">
+
     </div>
 </template>
 
@@ -14,7 +18,8 @@ import {fabric}  from "fabric"
 
 export default {
    data: () => ({
-       canvas: ''
+       canvas: '',
+       img: ''
     }),
     mounted() {
        console.log("iii")
@@ -29,8 +34,29 @@ export default {
                 width: 20,
                 height: 20
             });
+            console.log("Uem é img ", this.$refs["img"])
+            const img = new fabric.Image(this.$refs["img"], {
+                    left: 200,
+                    top: 200,
+                    angle: 0,
+                    opacity: 1
+                });
             this.canvas.add(rect)
+            this.canvas.add(img)
         }
     }
 }
 </script>
+
+<style scoped>
+.canvas {
+    border:1px solid black;
+   	position: absolute;
+    margin-left: 200px; /* This is half the width */
+	margin-top: -50px; /* This is half the height */
+	
+}
+
+</style>
+
+
