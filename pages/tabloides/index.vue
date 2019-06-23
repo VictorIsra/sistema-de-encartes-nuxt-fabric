cb49bd15482889b48377180d593d535601e7a162
victor israel <victorzen2013@gmail.com>
2019-06-22 23:30:07 -0300

    fabric funcionando finalmente! :)

--- pages/tabloides/index.vue
+++ pages/tabloides/index.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
-      uauauaua
-      </div>
+        <v-btn color="success" @click="criaRet">testa</v-btn>
+        <canvas id="c"></canvas>
+    </div>
 </template>
 
 <script>
-import fabric  from "fabric"
+import {fabric}  from "fabric"
 //const canvas = new fabric.Canvas('c')
 
 
@@ -13,10 +14,23 @@ import fabric  from "fabric"
 
 export default {
    data: () => ({
-  
+       canvas: ''
     }),
     mounted() {
        console.log("iii")
+       this.canvas = new fabric.Canvas('c');
+    },
+    methods: {
+        criaRet(){
+            var rect = new fabric.Rect({
+                left: 100,
+                top: 100,
+                fill: 'red',
+                width: 20,
+                height: 20
+            });
+            this.canvas.add(rect)
+        }
     }
 }
 </script>
