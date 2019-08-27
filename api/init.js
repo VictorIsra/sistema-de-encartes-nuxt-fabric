da49e965dbbc90350f68168b1350eb8f47fd1f85
Giancarllo Rojas <giancarllor@gmail.com>
2019-08-27 16:23:28 -0300

    proxy

--- api/init.js
+++ api/init.js
@@ -2,7 +2,10 @@ import axios from 'axios'
 import cookies from 'js-cookie'
 import {setAuthToken, resetAuthToken} from '~/utils/auth'
 
-axios.defaults.baseURL = 'http://localhost:3000/backend/'
+if(process.env.NODE_ENV == 'production')
+    axios.defaults.baseURL = 'http://35.194.25.104/backend/'
+else
+    axios.defaults.baseURL = 'http://localhost:3000/backend/'
 const token = cookies.get('x-access-token')//fundamental, sem isso ele "esquece o token" dps de um refresh/nova requisicao, msm com o cookie no cliente
 //mas lembre q ele só executar esse arquivo em casa req se eu usar o  nuxtServerInit
 if(token){
