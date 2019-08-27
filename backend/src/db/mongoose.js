c0e2fabfbc591e22e4b45447c42f0acdbae5fde0
Giancarllo Rojas <giancarllor@gmail.com>
2019-08-27 16:04:17 -0300

    rsrs

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 if(process.env.NODE_ENV == 'production'){
     //se conecta com o bd
-    mongoose.connect('mongodb://root:admin123@35.194.25.104:27017/encarte-api/?authSource=admin',{
+    mongoose.connect('mongodb://root:admin123@35.194.25.104:27017/encarte-api?authSource=admin',{
         useNewUrlParser: true,
         useCreateIndex: true ,//cria o index automaticamente, bem nice
         useFindAndModify: false
