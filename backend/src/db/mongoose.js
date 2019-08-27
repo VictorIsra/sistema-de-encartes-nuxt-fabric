6f819ebce369b24d9470ab90dc9d9b1067951875
Giancarllo Rojas <giancarllor@gmail.com>
2019-08-27 16:00:31 -0300

    rearranjando build/start

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-if(process.env == 'PROD'){
+if(process.env.NODE_ENV == 'production'){
     //se conecta com o bd
     mongoose.connect('mongodb://root:admin123@35.194.25.104:27017/encarte-api/?authSource=admin',{
         useNewUrlParser: true,
