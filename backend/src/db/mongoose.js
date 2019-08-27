bdce196c93941d520dbd50f78d9542af47c73c93
Giancarllo Rojas <giancarllor@gmail.com>
2019-08-27 15:52:45 -0300

    string de conexao

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose')
-//se conecta com o bd
-mongoose.connect('mongodb://127.0.0.1:27017/encarte-api',{
-    useNewUrlParser: true,
-    useCreateIndex: true ,//cria o index automaticamente, bem nice
-    useFindAndModify: false
-    
-})
+
+if(process.env == 'PROD'){
+    //se conecta com o bd
+    mongoose.connect('mongodb://root:admin123@35.194.25.104:27017/encarte-api/?authSource=admin',{
+        useNewUrlParser: true,
+        useCreateIndex: true ,//cria o index automaticamente, bem nice
+        useFindAndModify: false
+        
+    })
+}else{
+    //se conecta com o bd
+    mongoose.connect('mongodb://127.0.0.1:27017/encarte-api',{
+        useNewUrlParser: true,
+        useCreateIndex: true ,//cria o index automaticamente, bem nice
+        useFindAndModify: false
+        
+    })
+}
   
